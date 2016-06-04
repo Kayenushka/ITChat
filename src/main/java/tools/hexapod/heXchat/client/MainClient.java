5ac02cf6b6c08c7638cf145bef2cb76c8606a778
Igor Tkanka <kayen@ukr.net>
2016-06-04 23:07:52 +0300

    1

---
+++ src/main/java/tools/hexapod/heXchat/client/MainClient.java
@@ -0,0 +1,10 @@
+package tools.hexapod.heXchat.client;
+
+public class MainClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/tools/hexapod/heXchat/server/Main.java
@@ -0,0 +1,10 @@
+package tools.hexapod.heXchat.server;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
